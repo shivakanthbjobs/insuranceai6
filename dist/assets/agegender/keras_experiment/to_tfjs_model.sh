fec7e1d1ce54bd00857edf55dfb73484622abc61
Shiv Buddha <shivakanthb@gmail.com>
2018-09-30 01:54:06 +0530

    working with pose net and agegender with stop and start butto

--- dist/assets/agegender/keras_experiment/to_tfjs_model.sh
+++
@@ -1,3 +0,0 @@
-mkdir tfjs_model
-rm tfjs_model/* -fr
-tensorflowjs_converter --input_format keras keras_model.h5 tfjs_model
--- dist/assets/agegender/serve.py
+++
@@ -1,7 +0,0 @@
-from sanic import Sanic
-
-app = Sanic()
-app.static('/', 'dist')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
